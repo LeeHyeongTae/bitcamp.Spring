5838c4b70996fcc50df0e1bb02dbfad3d80294ec
LeeHyengTae <tedd911@icloud.com>
2020-04-17 18:12:30 +0900

    18:12

---
+++ java/com/leeht/web/LeehtApplication.java
@@ -0,0 +1,13 @@
+package com.leeht.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeehtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeehtApplication.class, args);
+	}
+
+}
