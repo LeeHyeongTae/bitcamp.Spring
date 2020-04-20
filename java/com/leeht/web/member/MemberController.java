5d473cf1751f953b066cef63c0f619a4dfea625a
LeeHyengTae <tedd911@icloud.com>
2020-04-20 17:58:26 +0900

    17:58

--- java/com/leeht/web/member/MemberController.java
+++ java/com/leeht/web/member/MemberController.java
@@ -1,5 +1,19 @@
 package com.leeht.web.member;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //url을 처리하는 @.
 public class MemberController {
+	public MemberService memberService;
 	
+	@PostMapping("/join")
+	public String add(@RequestBody Member member) {//controller는 무조건 응답이 있어야함!!
+		System.out.println(">>>>");
+		System.out.println(member.toString());
+		memberService = new MemberServiceImpl();
+		memberService.add(member);
+		return member.getName();
+	}
 }
