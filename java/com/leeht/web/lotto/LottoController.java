ca22445597b3ec3367b18105e017f443c9766537
LeeHyengTae <tedd911@icloud.com>
2020-04-22 20:58:39 +0900

    20:58

--- java/com/leeht/web/lotto/LottoController.java
+++ java/com/leeht/web/lotto/LottoController.java
@@ -2,6 +2,7 @@ package com.leeht.web.lotto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +23,8 @@ public class LottoController {
 		return (lottoService.count()==count+1)? Messenger.SUCCESS:Messenger.FAIL;
 	}
 	
-	@GetMapping("/result{userid}")
-	public LottoResult result(@RequestBody String userid) {
-		System.out.println(userid);
+	@GetMapping("/result/{userid}")
+	public LottoResult result(@PathVariable String userid) {
 		return lottoService.result(userid);
 	}
 
